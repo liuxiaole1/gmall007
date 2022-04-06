d9b08cc4235703495ced1c6dc17f2931d93c743d
xiaole <17634985200@163.com>
2022-04-06 11:06:27 +0800

    张开翔是大潮吧

--- abab/text.java
+++ abab/text.java
@@ -2,10 +2,10 @@
  * @program: gmall007
  * @description:
  * @author: Mr.liu
- * @create: 2022-04-06 10:43
+ * @create: 2022-04-06 10:57
  **/
-public class eee {
+public class text {
     public static void main(String[] args) {
-        System.out.println("abab");
+        System.out.println("张开翔hhh");
     }
 }
