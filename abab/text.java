0069c5563d8632d25bac97859b79121ff6afc5f0
xiaole <17634985200@163.com>
2022-04-07 09:43:18 +0800

    张开翔是大潮吧

--- abab/text.java
+++ abab/text.java
@@ -8,5 +8,6 @@ public class text {
     public static void main(String[] args) {
         System.out.println("张开翔hhh");
         System.out.println("456");
+        System.out.println("456");
     }
 }
